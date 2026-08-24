1f1d650fcce368df9b5bbda426afbc4d9806952e
YV3507 <hsk3510857887@163.com>
2026-08-24 09:07:13 +0800

    Revert "puppet cropoffset 交集规则 + FBO 裁剪: 头不再被身体盖"
    This reverts the puppet cropoffset 交集规则 + FBO 裁剪 change.

--- scripts/core/bump-to-sf19.mjs
+++
@@ -1,7 +0,0 @@
-// 缓存键 bump: sf18 → sf19 (puppet cropoffset 交集规则 + FBO 裁剪)
-import fs from 'node:fs';
-const p = 'lib/index.js';
-let s = fs.readFileSync(p, 'utf8');
-s = s.replace('sf18_', 'sf19_');
-fs.writeFileSync(p, s);
-console.log('缓存键:', [...new Set(s.match(/sf\d+_/g))].join(', '));
